09fbcc990124f7d68396d823ace07f4d5228380c
Sumin Choy <suminzero@gmail.com>
2023-01-28 15:05:47 -0800

    creating markers

--- HealthGo/static/common.js
+++ HealthGo/static/common.js
@@ -1,9 +1,15 @@
 let map;
 
 function initMap() {
+  const myLatlng = { lat: -34.397, lng: 150.644 }
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: myLatlng,
     zoom: 8,
+  });
+  const marker = new google.maps.Marker({
+    position: myLatlng,
+    map,
+    title: "Click to zoom",
   });
 }
 
