d752a775ce82ae6ab38330b53c18ec24b3175356
Sumin Choy <suminzero@gmail.com>
2023-01-28 21:31:31 -0800

    created the questions when clicked on waypoint

--- HealthGo/static/common.js
+++ HealthGo/static/common.js
@@ -1,17 +1,66 @@
 let map;
 
-function initMap() {
-  const myLatlng = { lat: -34.397, lng: 150.644 }
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: myLatlng,
-    zoom: 8,
-  });
-  const marker = new google.maps.Marker({
-    position: myLatlng,
-    map,
-    title: "Click to zoom",
-  });
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
 }
 
-window.initMap = initMap;
+//usage:
 
+function attachSecretMessage(marker, secretMessage) {
+    const infowindow = new google.maps.InfoWindow({
+      content: secretMessage,
+    });
+  
+    marker.addListener("click", () => {
+      infowindow.open(marker.get("map"), marker);
+    });
+}
+function initMap() {
+    const myLatlng = { lat: 37.242360, lng: -121.888214}, 
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: myLatlng,
+      zoom: 20,
+    });
+    readTextFile("./static/geo.json", function(text){
+      var data = JSON.parse(text);
+      console.log(data);
+      for(x in data){
+        const contentString =
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        "</div>" +
+        '<h1 style = "padding:20px;"id="firstHeading" class="firstHeading">Problem/Fun fact!</h1>' +
+        '<div id="bodyContent">' +
+        "<p> " +
+        data[x].question+
+        "</p>" +
+        "<input type=\"text\" id=\"\">" + 
+        "<button class= \"button4\" onclick=>Click Me</button>" +
+        "</div>" +
+        "</div>";
+          console.log(data[x].latitude)
+          console.log(typeof data[x].latitude)
+          console.log(data[x].longitude)
+          const marker = new google.maps.Marker({
+              position: {lat: Number(data[x].latitude), lng:Number(data[x].longitude)},
+              map,
+              title: "Waypoint: " + data[x].id,
+              
+            });
+            attachSecretMessage(marker, contentString);
+      }
+   
+  });
+    
+  }
+  
+  window.initMap = initMap;
+  
